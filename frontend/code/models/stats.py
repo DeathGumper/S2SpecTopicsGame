f96a675adc1e5f206581cb89da2d0f0d6ecdcc50
Hodges <s-hodgesja@bsd405.org>
2026-03-17 13:42:53 -0700

    Added stats that were not previously there to the stats class in the frontend
    Simplified the adjustment methods in the backend stats class

--- frontend/code/models/stats.py
+++ frontend/code/models/stats.py
@@ -1,7 +1,10 @@
 from pydantic import BaseModel
 
 class Stats(BaseModel):
+    maxHealth: float
     health: float
     strength: float
+    defense: float
     dexterity: float
     speed: float
+    accuracy: float
