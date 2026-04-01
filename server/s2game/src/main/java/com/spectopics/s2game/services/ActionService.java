7a664df10ed059b2831ad7fef75ec22be2497ba9
Hodges <s-hodgesja@bsd405.org>
2026-04-01 11:55:25 -0700

    Added code to call actions.
    Shifted the code to kill creatures to its own method for testing

--- server/s2game/src/main/java/com/spectopics/s2game/services/ActionService.java
+++ server/s2game/src/main/java/com/spectopics/s2game/services/ActionService.java
@@ -1,13 +1,22 @@
 package com.spectopics.s2game.services;
 
+import java.lang.reflect.InvocationTargetException;
+
 import com.spectopics.s2game.enums.BattleState;
 import com.spectopics.s2game.models.Battle;
 
 public class ActionService {
     public static void CallAction(Battle battle, String act) {
-        // TODO: jacobs code
+        String[] parts = act.split("-");
+        try {
+            ActionService.class.getMethod(parts[0],Battle.class, String.class).invoke(null,battle,act);
+        } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {
+            System.out.println("Error: " + e);
+            return;
+        }
+    }
 
-        // TEMPORARY
+    public static void KillCreature(Battle battle, String act) {
         if (battle.GetBattleState() == BattleState.PLAYER1) {
             battle.NextP2Creature();
             battle.NextTurn();
