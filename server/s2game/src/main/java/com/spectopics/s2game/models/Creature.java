169ce44cddc4f7e87e19465f8b0286bfaaeccf75
DeathGumper <baynewilson9@gmail.com>
2026-04-01 11:41:06 -0700

    Stupid dumb git thing >:(

--- server/s2game/src/main/java/com/spectopics/s2game/models/Creature.java
+++ server/s2game/src/main/java/com/spectopics/s2game/models/Creature.java
@@ -1,51 +1,31 @@
 package com.spectopics.s2game.models;
-
+ 
+ 
 import lombok.Data;
-
+ 
 @Data
 public class Creature {
     private String name;
     private Stats stats;
     private float totalSpeed;
-
+ 
     public float addTotalSpeed(float speed) {
         this.totalSpeed += speed;
         return this.totalSpeed;
     }
-
+ 
     public void resetTotalSpeed() {
         this.totalSpeed = 0;
         addTotalSpeed(this.stats.getSpeed());
     }
-
-<<<<<<< HEAD
-=======
-    public void NextTurn() {
-        /*
-        Updates variables, calls methods, and other things
-         that need to be done at the start of each turn
-        */
-
-        //TODO
-        /*
-        Current list of what to add here:
-            Damage/Heal Over Time
-        */
-    }
-
-
-
-    // Creature declaration stuff
-    public static HashMap<String, Creature> creatures = new HashMap<>();
-    public static void AddNew(Creature creature) {
-        creatures.put(creature.getName().replaceAll(" ", "").toLowerCase(), creature);
-    }
-
-    public Creature GetNew(String name) {
-        return creatures.get(name).copy();
-    }
-
->>>>>>> main
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
     public Creature copy() {
         Creature newCreature = new Creature();
         newCreature.setName(this.name);
